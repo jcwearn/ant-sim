c13b1289d81bb8337226bde18caaffd0655786d6
Jackson Wearn <jackson.wearn@careerbuilder.com>
2015-10-14 16:31:38 -0400

    Add path using path module rather than as string

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
 var express = require('express'),
-    CSVParser = require('../lib/parse-csv.js');
+    CSVParser = require('../lib/parse-csv.js'),
+    path = require('path');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  CSVParser.parseFile('../ants.csv', function(err, data) {
+  var csv_path = path.join(__dirname, '../ants.csv');
+  CSVParser.parseFile(csv_path, function(err, data) {
     if(err) console.log(err);
     if(data) res.render('index', { csv: data });
   });
