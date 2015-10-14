332843de14e9720b0de4e4dc1c82cbf6528c226f
Jackson Wearn <jackson.wearn@careerbuilder.com>
2015-10-14 14:53:48 -0400

    Add ant trail object, rename variables with row in name to ant, breakout canvas drawing code into helper functions

--- public/javascripts/canvas-helper.js
+++ public/javascripts/canvas-helper.js
@@ -7,42 +7,68 @@ var img = new Image();
 img.src = '../images/ant.png';
 
 var timestamp = 66;
-var nextRow = 0;
+var nextAnt = 0;
+
+var antTrails = {
+  ant1: [],
+  ant2: [],
+  ant3: [],
+  ant10: [],
+  ant33: [],
+  ant37: [],
+  ant42: [],
+  ant45: []
+};
 
 function draw(csv){
   var interval = setInterval(function(){
-    renderFrame(csv.slice(nextRow));
+    renderFrame(csv.slice(nextAnt));
   }, 33);
 }
 
 function renderFrame(csv) {
-  var rowsToDraw = [];
+  var antsToDraw = [];
   for(var i = 1; i < csv.length; i++) {
-    var row = csv[i];
-    var timestamp_current = row[1];
+    var ant = csv[i];
+    var timestamp_current = ant[1];
     
     if(timestamp_current === timestamp) {
-      rowsToDraw.push({
-        id: row[0],
-        timestamp: row[1],
-        x: row[2],
-        y: row[3],
-        angle: row[4]
+      antsToDraw.push({
+        id: ant[0],
+        timestamp: ant[1],
+        x: ant[2],
+        y: ant[3],
+        angle: ant[4]
       });
     } else {
-      nextRow += i;
+      nextAnt += i;
       timestamp = timestamp_current;
+      
       ctx.clearRect(0, 0, canvas.width, canvas.height);
-      rowsToDraw.forEach(function(row) {
-        ctx.save();
-        ctx.translate(row.x, row.y);
-        ctx.translate(8, 8);
-        ctx.rotate(row.angle);
-        ctx.drawImage(img, -8, -8);
-        ctx.restore();
+      
+      antsToDraw.forEach(function(row) {
+        drawAnt(ant);
       });
 
       break;
     }
   }
+}
+
+function drawAnt(ant) {
+  var x = ant.x;
+  var y = ant.y;
+  var angle = ant.angle;
+  var id = ant.id;
+  
+  ctx.save();
+  ctx.translate(x, y);
+  ctx.translate(8, 8);
+  ctx.rotate(angle);
+  ctx.drawImage(img, -8, -8);
+  ctx.restore();
+}
+
+function drawTrail() {
+  
 }
