c989ba555d37d8aad997d81e00828e9eeb9fd504
Jackson Wearn <jackson.wearn@careerbuilder.com>
2015-10-13 23:10:00 -0400

    Basic csv parsing

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
-var express = require('express');
+var express = require('express'),
+    CSVParser = require('../lib/parse-csv.js');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  CSVParser.parseFile('../ants.csv', function(err, data) {
+    if(err) console.log(err);
+    if(data) {
+      data.forEach(function(row) {
+        console.log(row);
+      });
+    }
+  });
+
   res.render('index', { title: 'Express' });
+
 });
 
 module.exports = router;
